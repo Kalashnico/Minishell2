fad04bfbc3b4ce80cd6f4f4317b730315f13e845
Kalashnico <nicolas.guerin@epitech.eu>
2017-04-06 23:44:54 +0200

    norme 1.0

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login   <nicolas.guerin@epitech.eu>
 ** 
 ** Started on  Sun Apr  2 06:15:14 2017 Nicolas
-** Last update Thu Apr  6 16:38:17 2017 Nicolas
+** Last update Thu Apr  6 23:44:09 2017 Nicolas
 */
 
 #include "prototypes.h"
@@ -15,12 +15,39 @@ void	prompt()
   my_putstr("$>", 0);
 }
 
+char	**mysh(char **env, char *buff, char **new_av, int *ret)
+{
+  if (check_if_built_exist(buff) == 0)
+    {
+      if ((builtin(buff, env)) == 84 ||
+	  (env = builtin_env(buff, env)) == NULL)
+	{
+	  my_putstr("Error in the execution of the command\n", 2);
+	  *ret = 1;
+	}
+    }
+  else
+    {
+      if ((my_execve_brut(buff, new_av, env)) == 84)
+	if (my_execve(new_av, env) == 84 && my_strlen(buff) != 0)
+	  {
+	    my_putstr(new_av[0], 2);
+	    my_putstr(": Command not found.\n", 2);
+	   *ret = 1;
+	  }
+    }
+  free_tab(new_av);
+  free(buff);
+  prompt();
+  return (env);
+}
+
 int	main(int ac,__attribute__ ((unused)) char **av, char **env)
 {
   char	*buff;
   char	**new_av;
   int	ret;
-
+  
   if (ac != 1)
     return (my_putstr("Too many arguments !\n", 2), 84);
   prompt();
@@ -31,30 +58,12 @@ int	main(int ac,__attribute__ ((unused)) char **av, char **env)
       if ((buff = epur_str(buff)) == NULL ||
 	  (new_av = my_str_to_wordtab(buff, ' ')) == NULL)
 	return (84);
-      if ((my_strcmp("exit", new_av[0]) == 0))
-	return (my_getnbr(new_av[1]));
-      else if (check_if_built_exist(buff) == 0)
-	{
-	  if ((builtin(buff, env)) == 84 ||
-	      (env = builtin_env(buff, env)) == NULL)
-	    {
-	      my_putstr("Error in the execution of the command\n", 2);
-	      ret = 1;
-	    }
-	}
-      else
-	{
-	  if ((my_execve_brut(buff, new_av, env)) == 84)
-	    if (my_execve(new_av, env) == 84 && my_strlen(buff) != 0)
-	      {
-		my_putstr(new_av[0], 2);
-		my_putstr(": Command not found.\n", 2);
-		ret = 1;
-	      }
-	}
-      free_tab(new_av);
-      free(buff);
-      prompt();
+       if ((my_strcmp("exit", new_av[0]) == 0))
+	 return (my_getnbr(new_av[1]));
+       if ((env = mysh(env, buff, new_av, &ret)) == NULL)
+	 return (84);
+       printf("ret = %d\n", ret);
     }
+  free(new_av);
   return (ret);
 }
